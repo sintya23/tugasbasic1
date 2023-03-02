ebb7ec1c53c6ffaa7358b5ccb7a9945c02fb1181
unknown <sintyakumara191005@gmail.com>
2023-03-02 16:30:19 +0800

    first commit

--- class.php
+++ class.php
@@ -1,16 +1,15 @@
 <?php
-class luas{
+class luas
+{
 
-public function segitiga($alas, $tinggi){
-   return 1/2 * $alas * $tinggi;
- }
-
+    public function segitiga($alas, $tinggi)
+    {
+        return 1 / 2 * $alas * $tinggi;
+    }
 }
 
 $luas = new Luas();
 
 $alas = 10;
-    $tinggi = 15;
-    echo $luas->segitiga($alas, $tinggi);
-
-?>
+$tinggi = 15;
+echo $luas->segitiga($alas, $tinggi);
--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-$alas=4;
-$tinggi=6;
-$luas=($alas*$tinggi)/2;
+$alas = 4;
+$tinggi = 6;
+$luas = ($alas * $tinggi) / 2;
 
 echo "Hasil dari rumus Luas segitiga dengan rumus 1/2 (alas*tinggi). <br>
 alas = $alas <br>
 tinggi = $tinggi <br>
 luas = $luas";
-
-?>
